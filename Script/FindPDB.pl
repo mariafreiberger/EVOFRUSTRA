4c189cd2008c49206e2b59929598c1dfd7d4b93d
mariafreiberger <37904042+mariafreiberger@users.noreply.github.com>
2018-04-27 16:07:32 -0300

    Update FindPDB.pl

--- Script/FindPDB.pl
+++ Script/FindPDB.pl
@@ -1,50 +1,11 @@
 use strict;
 
 open(blista,"@ARGV[0]/OutPutFiles@ARGV[1]/Sequences.fasta");
-open(ListaBlast,">@ARGV[0]/OutPutFiles@ARGV[1]/ListaPDB.txt");
-open(error,">@ARGV[0]/OutPutFiles@ARGV[1]/SinPDB.txt");
-open(outfasta,">@ARGV[0]/OutPutFiles@ARGV[1]/SequencesComplete.fasta");
-
-my $e=1;
 
 while(my $lineB=<blista>){
 	chomp $lineB;
 	$lineB =~ tr/a-z/A-Z/;
-	my @splitter= split "", $lineB;
-	system("awk '\$2 ~ /$lineB/' @ARGV[0]/pdb_seq.txt > @ARGV[0]/OutPutFiles@ARGV[1]/Listaawk.txt");
-	open (awk,"@ARGV[0]/OutPutFiles@ARGV[1]/Listaawk.txt");
-	my $linepdb=<awk>;
-	my @splitpdb=split " ",$linepdb;
-	if (@splitpdb[0]eq""){
-		my $tam=@splitter;
-		my $lineBc=substr($lineB,0,$tam/2);
-		my $lineBf=substr($lineB,$tam/2,$tam);
-		system("awk '\$2 ~ /my $lineBc/' @ARGV[0]/pdb_seq.txt > @ARGV[0]/OutPutFiles@ARGV[1]/Listaawk.txt");
-		open (awk,"@ARGV[0]/OutPutFiles@ARGV[1]/Listaawk.txt");
-		my $linepdb=<awk>;
-		my @splitpdb=split " ",$linepdb;
-			if (@splitpdb[0]eq""){
-				system("awk '\$2 ~ /my $lineBf/' @ARGV[0]/pdb_seq.txt > @ARGV[0]/OutPutFiles@ARGV[1]/Listaawk.txt");
-				open (awk,"@ARGV[0]/OutPutFiles@ARGV[1]/Listaawk.txt");
-				my $linepdb=<awk>;
-				my @splitpdb=split " ",$linepdb;
-				if (@splitpdb[0]eq""){
-					print ListaBlast ">seqerror$e\n";
-					print error "$lineB\n";
-					$e++;		
-					}
-				else{
-					print ListaBlast "@splitpdb[0]\n";
-					}
-				}
-			else{
-				print ListaBlast "@splitpdb[0]\n";
-		}
-	}
-	else{
-		print ListaBlast "@splitpdb[0]\n";
-		}
-	close(awk);
+	system("awk '\$2 ~ /$lineB/' @ARGV[0]/DBSeq.fasta >> @ARGV[0]/OutPutFiles@ARGV[1]/ListaPDB.txt");
 }
-
 close(blista);
+close(seq);
