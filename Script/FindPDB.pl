d642c2c839fb6aa5d6ed6525f0f132f0f0b7def2
mariafreiberger <37904042+mariafreiberger@users.noreply.github.com>
2018-04-27 19:39:26 -0300

    Update FindPDB.pl

--- Script/FindPDB.pl
+++ Script/FindPDB.pl
@@ -1,11 +1,16 @@
 use strict;
 
 open(blista,"@ARGV[0]/OutPutFiles@ARGV[1]/Sequences.fasta");
+open(salida,">@ARGV[0]/OutPutFiles@ARGV[1]/ListaPDB.txt");
 
 while(my $lineB=<blista>){
 	chomp $lineB;
 	$lineB =~ tr/a-z/A-Z/;
-	system("awk '\$2 ~ /$lineB/' @ARGV[0]/DBSeq.fasta >> @ARGV[0]/OutPutFiles@ARGV[1]/ListaPDB.txt");
+	system("awk '\$2 ~ /$lineB/' @ARGV[0]/DBSeq.fasta > @ARGV[0]/OutPutFiles@ARGV[1]/Listaawk.txt");
+	open (awk,"@ARGV[0]/OutPutFiles@ARGV[1]/Listaawk.txt");
+	my $linepdb=<awk>;
+	my @spl=split " ",$linepdb;
+	print salida "@spl[0]\n";
 }
 close(blista);
 close(seq);
