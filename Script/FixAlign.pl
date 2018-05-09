b09aec34cea4c9004497af17d2cbdb1b62e5fe1d
mariafreiberger <37904042+mariafreiberger@users.noreply.github.com>
2018-05-09 16:02:22 -0300

    Update FixAlign.pl

--- Script/FixAlign.pl
+++ Script/FixAlign.pl
@@ -2,13 +2,33 @@ use strict;
 
 #/home/maria/Desktop/AutoLogo/OutPutFilesCDD15489
 
-open(alin,"@ARGV[0]/OutPutFiles@ARGV[1]/AlignSE.fasta");
-open(sal,">@ARGV[0]/OutPutFiles@ARGV[1]/Alignment.fasta");
+open(alin,"@ARGV[0]/OutPutFiles@ARGV[1]/SeqAlign.fasta");
+open(sal,">@ARGV[0]/OutPutFiles@ARGV[1]/AlignSE.fasta");
 open(lista,"@ARGV[0]/OutPutFiles@ARGV[1]/ListaPDB.txt");
 
 my $c=0;
+my $d=0;
 
-while(my $line=<alin>){
+my $line=<alin>;
+chomp $line;
+my @spline=split "", $line;
+my $sq="@spline[4]@spline[5]";
+my $r=0;
+
+while(my $lista=<lista>){
+	chomp $lista;
+	$r++;
+	if($r eq $sq){	
+		$line=<alin>;
+		print sal ">$lista\n$line";
+	}
+}
+
+close(lista);
+
+open(lista,"@ARGV[0]/OutPutFiles@ARGV[1]/ListaPDB.txt");
+while($line=<alin>){
+	$c++;
 	my $lista=<lista>;
 	chomp $lista;
 	my @splista=split "", $lista; 
