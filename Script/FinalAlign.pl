bdb20c5e2e4d2e0cd18c6d9d7164f6394661d2ee
mariafreiberger <37904042+mariafreiberger@users.noreply.github.com>
2018-05-09 16:00:02 -0300

    Update FinalAlign.pl

--- Script/FinalAlign.pl
+++ Script/FinalAlign.pl
@@ -1,6 +1,6 @@
 use strict;
 
-open(align,"@ARGV[0]/OutPutFiles@ARGV[1]/SeqAlign3.fasta");
+open(align,"@ARGV[0]/OutPutFiles@ARGV[1]/SeqAlign4.fasta");
 open(nuevo,">@ARGV[0]/OutPutFiles@ARGV[1]/SalidaAlign.fasta");
 open(nuevo2,">@ARGV[0]/OutPutFiles@ARGV[1]/SalidaAlignSE.fasta");
 open(posi,">@ARGV[0]/OutPutFiles@ARGV[1]/Posiciones");
@@ -10,8 +10,11 @@ my @vector;
 my $m=0;
 my $j;
 my $control=0;
+my @splres;
 
 while(my $align=<align>){
+	my $sres;
+	my $co;
 	if ($c==0){
 		$c++;}
 	else{	
@@ -25,22 +28,25 @@ while(my $align=<align>){
 		{	$c++;
 			my $i=0;
 			while($i<$tam){
-				if(@splitter[$i]eq"-"){
-					@vector[$i]=1;
+				if((@splitter[$i]eq"-") or (@splitter[$i]eq"Z")){
+					@vector[$i]=0;
 					}
 				else{
-					@vector[$i]=0;
+					@vector[$i]=1;
 					}
 				$i++;
 				}
 			}
 		else{
 			if(@splitter[0]eq">"){
+				open(Sres,"@ARGV[0]/OutPutFiles@ARGV[1]/Modeller/@splitter[1]@splitter[2]@splitter[3]@splitter[4]_@splitter[6].pdb.done/FrustrationData/@splitter[1]@splitter[2]@splitter[3]@splitter[4].pdb_singleresidue");
+					$sres=<Sres>;
 				if($m==0){
 					print nuevo ">@splitter[1]@splitter[2]@splitter[3]@splitter[4]_@splitter[6]\n";
 					print nuevo2 ">@splitter[1]@splitter[2]@splitter[3]@splitter[4]_@splitter[6]\n";	
 					print posi ">@splitter[1]@splitter[2]@splitter[3]@splitter[4]_@splitter[6]\n";
 					$m++;
+					
 					}
 				else{
 					print nuevo "\n>@splitter[1]@splitter[2]@splitter[3]@splitter[4]_@splitter[6]\n";
@@ -50,41 +56,52 @@ while(my $align=<align>){
 		
 			}	
 			else {
+				my $tam = @splitter;
 				my $j=0;
 				my $n=0;
-				my $co=0;
+				my $q=0;
 				while ($j<$tam-1){
 					if(@vector[$j]==0){
-						if(@splitter[$j]eq"-"){
-							print posi "0 ";
+						if(@splitter[$j] ne "-"){	
+							$sres=<Sres>;}
+						}
+					else{
+						if(@splitter[$j]eq"Z"){
+							print nuevo "-";
+							$n++; $q++;
+							print nuevo2 "-";
 							}
 						else{
-							if(@splitter[$j]eq"-"){}
-							else {$co++;}
-							print posi "$co ";
+							print nuevo "@splitter[$j]";
+							$n++; $q++;
+							print nuevo2 "@splitter[$j]";
 							}
-						print nuevo "@splitter[$j]";
-						$n++;
-						print nuevo2 "@splitter[$j]";
 						if($n==51){
 							print nuevo "\n";
 							$n=0;			
-							}
-						}
-					else{
-						if(@splitter[$j]eq"-"){
-							}
-						else{
-								$co++;
 								}
+						if(@splitter[$j]eq"-"){
+							print posi "G ";
+							}
+						else{	
+							if(@splitter[$j]eq"Z"){
+								print posi "Z ";
+								$sres=<Sres>;
+								}
+							else{	
+								$sres=<Sres>;
+								@splres= split " ",$sres;
+								print posi "@splres[0] ";	
+								}	
+							}
+					
 						}
 					$j++;
+					
 					}
 				}
 			}
 		}
-
-	
 
 }
 
