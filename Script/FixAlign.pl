b0d19c531b6126bfd51438194c341e67b2444e76
mariafreiberger <37904042+mariafreiberger@users.noreply.github.com>
2018-05-29 14:01:14 -0300

    Update FixAlign.pl

--- Script/FixAlign.pl
+++ Script/FixAlign.pl
@@ -1,44 +1,19 @@
 use strict;
+use Bio::SeqIO;
 
-#/home/maria/Desktop/AutoLogo/OutPutFilesCDD15489
-
-open(alin,"@ARGV[0]/OutPutFiles@ARGV[1]/SeqAlign.fasta");
 open(sal,">@ARGV[0]/OutPutFiles@ARGV[1]/AlignSE.fasta");
 open(lista,"@ARGV[0]/OutPutFiles@ARGV[1]/ListaPDB.txt");
 
+my @vector;
 my $c=0;
-my $d=0;
+my $alignment="@ARGV[0]/OutPutFiles@ARGV[1]/Alignment.fasta";
 
-my $line=<alin>;
-chomp $line;
-my @spline=split "", $line;
-my $sq="@spline[4]@spline[5]";
-my $r=0;
-
-while(my $lista=<lista>){
-	chomp $lista;
-	$r++;
-	if($r eq $sq){	
-		$line=<alin>;
-		print sal ">$lista\n$line";
-	}
-}
-
-close(lista);
-
-open(lista,"@ARGV[0]/OutPutFiles@ARGV[1]/ListaPDB.txt");
-while($line=<alin>){
-	$c++;
+my $secuencia = Bio::SeqIO -> new(
+	-format => "fasta",
+	-file => "$alignment");
+while (my $SEQ = $secuencia->next_seq()){
 	my $lista=<lista>;
-	chomp $lista;
-	my @splista=split "", $lista; 
-	my @sp=split "", $line;
-	$line=<alin>;
-	if($splista[0]eq">"){
-		}		
-	else{	
-		print sal ">$lista\n$line";
-		}
+	print sal ">$lista",$SEQ->seq(),"\n";
 }
 
 close(alin);
