4db1b0c2d33344cb2b54d406386eada21eaa502e
mariafreiberger <37904042+mariafreiberger@users.noreply.github.com>
2018-05-29 14:00:16 -0300

    Update ChangeAlign.pl

--- Script/ChangeAlign.pl
+++ Script/ChangeAlign.pl
@@ -1,52 +1,54 @@
 use strict;
+use Bio::SeqIO;
 
-open(corregido,"@ARGV[0]/OutPutFiles@ARGV[1]/SeqAlign2.fasta");
 open(salida, ">@ARGV[0]/OutPutFiles@ARGV[1]/SeqAlign3.fasta");
 open(sali, ">@ARGV[0]/OutPutFiles@ARGV[1]/SeqAlign4.fasta");
 
 my $pdbid;
 my @sp;
 
-while(my $line=<corregido>){
-	chomp $line;
-	my $Sres;
-	my @splitter= split "",$line;	
-	if(@splitter[0] eq ">"){
-		print salida "$line";
-		print sali "$line";
-		open(Sres,"@ARGV[0]/OutPutFiles@ARGV[1]/Modeller/@splitter[1]@splitter[2]@splitter[3]@splitter[4]@splitter[5]@splitter[6].pdb.done/FrustrationData/@splitter[1]@splitter[2]@splitter[3]@splitter[4].pdb_singleresidue");
-		$Sres=<Sres>;
-		}
-	else{
-		$Sres=<Sres>;
-		my @splitres;
-		my $tam=@splitter;
-		my $c=0;
-		while($c<$tam){
-			@splitres=split " ", $Sres;
-			if((@splitter[$c] eq "-") or (@splitter[$c] eq "X")){
-				print salida "-";
-				print sali "-";
-				}	
-			else{	
-				if(@splitres[1] eq "Missing"){
-					print salida "-";
-					print sali "Z";
-					$Sres=<Sres>;
-				}
-				else {
-					print sali "@splitres[3]";
-					print salida "@splitres[3]";
-					$Sres=<Sres>;
-					}
-				}
-			$c++;
-			}
-		}
-	print salida "\n";
-	print sali "\n";
+my $alignment="@ARGV[0]/OutPutFiles@ARGV[1]/SeqAlign2.fasta";
 
+my $secuencia = Bio::SeqIO -> new(
+	-format => "fasta",
+	-file => "$alignment");
+
+while (my $SEQ = $secuencia->next_seq()){
+	my $c=0;
+	my $SEC=$SEQ->seq();
+	my @splitter= split "",$SEC;
+	my @spname=split "_",$SEQ->display_id;
+	open(Sres,"@ARGV[0]/OutPutFiles@ARGV[1]/Modeller/@spname[0]_@spname[1].pdb.done/FrustrationData/@spname[0].pdb_singleresidue");
+	print salida ">",$SEQ->display_id,"\n";
+	print sali ">",$SEQ->display_id,"\n";
+	my $Sres=<Sres>;
+	$Sres=<Sres>;
+	my $long=$SEQ->length;
+	my @splitres;
+	while($c<$long){
+		@splitres=split " ", $Sres;
+		if((@splitter[$c] eq "-") or (@splitter[$c] eq "X")){
+			print salida "-";
+			print sali "-";
+			}	
+		else{	
+			if(@splitres[1] eq "Missing"){
+				print salida "-";
+				print sali "Z";
+				$Sres=<Sres>;
+			}
+			else {
+				print sali "@splitres[3]";
+				print salida "@splitres[3]";
+				$Sres=<Sres>;
+				}
+			}
+		$c++;
+		}
+print sali "\n";
+print salida "\n";
 }
 
 close(corregido);
 close(salida);
+close(sali);
