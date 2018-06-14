1a71dd523555e576c0f505989d7d4c55533d975f
mariafreiberger <37904042+mariafreiberger@users.noreply.github.com>
2018-06-14 14:33:04 -0300

    Update RunAutoLogo.pl

--- RunAutoLogo.pl
+++ RunAutoLogo.pl
@@ -98,11 +98,11 @@ system ("perl $jobsDir/Script/Equivalences.pl $jobsDir $jobID");
 system("perl $jobsDir/Script/FastaMod.pl $jobsDir $jobID");
 copy ("$jobsDir/OutPutFiles$jobID/long.txt","$jobsDir/OutPutFiles$jobID/Equivalences/long.txt");
 system("cd $jobsDir/OutPutFiles$jobID/Equivalences; cat *SalidaSRes* > AllSalidaSResB.txt");
+system("perl $jobsDir/Script/LogoCheck.pl $jobsDir $jobID");
 system("cd $jobsDir/OutPutFiles$jobID/Equivalences; Rscript Logo.R");
 system("cd $jobsDir/OutPutFiles$jobID/Equivalences; Rscript Generator.R");
 system("cd $jobsDir/OutPutFiles$jobID/Equivalences; Rscript SeqLogo.R");
 system("perl $jobsDir/Script/VScript.pl $jobsDir $jobID");
-system("perl $jobsDir/Script/LogoCheck.pl $jobsDir $jobID");
 
 copy ("$jobsDir/OutPutFiles$jobID/Equivalences/HistogramFrustration.svg","$jobsDir/OutPut$jobID/HistogramFrustration.svg");
 
