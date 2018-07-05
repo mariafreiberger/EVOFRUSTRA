6a60ec70cbc2ade486eee723aca8c7aaf5d6220a
mariafreiberger <37904042+mariafreiberger@users.noreply.github.com>
2018-07-05 15:40:21 -0300

    Update FastaMod.pl

--- Script/FastaMod.pl
+++ Script/FastaMod.pl
@@ -4,12 +4,11 @@ open(ali,"@ARGV[0]/OutPutFiles@ARGV[1]/SalidaAlignSE.fasta");
 open(lista,">@ARGV[0]/OutPutFiles@ARGV[1]/Equivalences/Logo.fasta");
 
 while(my $line=<ali>){
-	chomp $line;
 	my @splitter= split "",$line;
 	if(@splitter[0] eq ">"){
 		}
 	else{
-		print lista "$line\n";
+		print lista "$line";
 		}
 }
 close(ali);
