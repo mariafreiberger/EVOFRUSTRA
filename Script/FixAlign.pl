0389660a30dfa82478c3689e1010be89d0c3e978
mariafreiberger <37904042+mariafreiberger@users.noreply.github.com>
2018-07-07 19:45:52 -0300

    Update FixAlign.pl

--- Script/FixAlign.pl
+++ Script/FixAlign.pl
@@ -1,19 +1,18 @@
 use strict;
-use Bio::SeqIO;
 
 open(sal,">@ARGV[0]/OutPutFiles@ARGV[1]/AlignSE.fasta");
 open(lista,"@ARGV[0]/OutPutFiles@ARGV[1]/ListaPDB.txt");
+open(align,"@ARGV[0]/OutPutFiles@ARGV[1]/Alignment.fasta");
 
-my @vector;
-my $c=0;
-my $alignment="@ARGV[0]/OutPutFiles@ARGV[1]/Alignment.fasta";
 
-my $secuencia = Bio::SeqIO -> new(
-	-format => "fasta",
-	-file => "$alignment");
-while (my $SEQ = $secuencia->next_seq()){
-	my $lista=<lista>;
-	print sal ">$lista",$SEQ->seq(),"\n";
+while (my $SEQ = <align>){
+	chomp $SEQ;
+	my @splitter= split "",$SEQ;
+	if((@splitter[0]eq">")or(@splitter[0]eq"")){}
+	else{
+		my $lista=<lista>;
+		print sal ">$lista",$SEQ,"\n";
+	}
 }
 
 close(alin);
