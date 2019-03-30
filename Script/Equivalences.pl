ae6286abe075bfd6e30e16fff79a210d4544db83
mariafreiberger <37904042+mariafreiberger@users.noreply.github.com>
2019-03-30 15:00:32 -0300

    Update Equivalences.pl

--- Script/Equivalences.pl
+++ Script/Equivalences.pl
@@ -10,13 +10,13 @@ while(my $align=<align>){
 		my @sp=split "_",$align;
 		my $t=@sp;
 		if($t>2){	
-			open(salida,">@ARGV[0]/OutPutFiles@ARGV[1]/Equivalences/SalidaSRes@splitalign[1]@splitalign[2]@splitalign[3]@splitalign[4]_@splitalign[6]_@sp[2].txt");
+			open(salida,">@ARGV[0]/OutPutFiles@ARGV[1]/Equivalences/SalidaSRes@splitalign[1]@splitalign[2]@splitalign[3]@splitalign[4]_@splitalign[6]_@sp[2].txt");#Create the Equival File
 			}
 		else{
-			open(salida,">@ARGV[0]/OutPutFiles@ARGV[1]/Equivalences/SalidaSRes@splitalign[1]@splitalign[2]@splitalign[3]@splitalign[4]_@splitalign[6].txt");
+			open(salida,">@ARGV[0]/OutPutFiles@ARGV[1]/Equivalences/SalidaSRes@splitalign[1]@splitalign[2]@splitalign[3]@splitalign[4]_@splitalign[6].txt"); #Create the Equival File
 			}
-		open(Sres,"@ARGV[0]/OutPutFiles@ARGV[1]/Modeller/@splitalign[1]@splitalign[2]@splitalign[3]@splitalign[4]_@splitalign[6].pdb.done/FrustrationData/@splitalign[1]@splitalign[2]@splitalign[3]@splitalign[4].pdb_singleresidue");
-		$Sres=<Sres>;
+		open(Sres,"@ARGV[0]/OutPutFiles@ARGV[1]/Modeller/@splitalign[1]@splitalign[2]@splitalign[3]@splitalign[4]_@splitalign[6].pdb.done/FrustrationData/@splitalign[1]@splitalign[2]@splitalign[3]@splitalign[4].pdb_singleresidue"); #Frustration file
+		$Sres=<Sres>; #Read the header
 		$pdb="@splitalign[1]@splitalign[2]@splitalign[3]@splitalign[4]";
 			}
 	else{
@@ -25,14 +25,15 @@ while(my $align=<align>){
 		my $tam=@splitalignes;
 		while($ter<$tam){
 			$ter++;
-			if((@splitalignes[$ter-1] eq "G") or (@splitalignes[$ter-1] eq "Z")){
+			if((@splitalignes[$ter-1] eq "G") or (@splitalignes[$ter-1] eq "Z")){#is a gap or a mr?
 				print salida "$ter	N/A	N/A	N/A	N/A\n";
+				if(@splitalignes[$ter-1] eq "Z"){$Sres=<Sres>;}#is mr read a line in frustration file
 						}
-			else{	
-				$Sres=<Sres>;
+			else{	#is a aminoacid
+				$Sres=<Sres>; #read a line i frustration file
 				chomp $Sres;
 				my @splitterSres=split " ",$Sres;
-				if(@splitterSres[0] != @splitalignes[$ter-1]){
+				if(@splitterSres[0] != @splitalignes[$ter-1]){ # check the numeration
 						while(){
 							$Sres=<Sres>;
 							chomp $Sres;
