ebbb93a835671426e82b1053310e1a398796863b
mariafreiberger <37904042+mariafreiberger@users.noreply.github.com>
2019-03-30 15:04:20 -0300

    Update FrustraPDB.pl

--- Script/FrustraPDB.pl
+++ Script/FrustraPDB.pl
@@ -4,6 +4,7 @@ use Bio::SeqIO;
 
 open(Lista,"@ARGV[0]/OutPutFiles@ARGV[1]/ListaPDB.txt");
 my @sp;
+my $pathFrustra="/home/maria/bin/Frustratometer/frustratometer2";
 
 while(my $PDB=<Lista>){
 	chomp $PDB;
@@ -12,7 +13,7 @@ while(my $PDB=<Lista>){
 	if(@sperror[0]eq">"){}	
 	else{		
 		my $tam=@sp;
-		copy("/home/maria/Desktop/PDBs/@sp[0].pdb","@ARGV[0]/OutPutFiles@ARGV[1]/@sp[0].pdb");
+		system("cp /home/maria/Desktop/PDBs/@sp[0].pdb @ARGV[0]/OutPutFiles@ARGV[1]/@sp[0].pdb");
 		#system ("wget 'http://www.rcsb.org/pdb/files/@sp[0].pdb' -O @ARGV[0]/OutPutFiles@ARGV[1]/@sp[0].pdb");
 		if($tam==2){
 			system ("perl @ARGV[0]/Script/ChainSeparate.pl @sp[0] @sp[1] @ARGV[0] @ARGV[1]");
@@ -20,12 +21,12 @@ while(my $PDB=<Lista>){
 		else{
 			system ("perl @ARGV[0]/Script/ChainSeparate.pl @sp[0] @sp[1] @ARGV[0] @ARGV[1] @sp[2] @sp[3]");
 			}
-		copy("@ARGV[0]/OutPutFiles@ARGV[1]/Modeller/@sp[0].pdb","/home/maria/bin/Frustratometer/frustratometer2/@sp[0]_@sp[1].pdb");
-		system ("cd /home/maria/bin/Frustratometer/frustratometer2; perl RunFrustratometer.pl @sp[0]_@sp[1].pdb singleresidue");	
-		move("/home/maria/bin/Frustratometer/frustratometer2/@sp[0]_@sp[1].pdb.done","@ARGV[0]/OutPutFiles@ARGV[1]/Modeller/@sp[0]_@sp[1].pdb.done");
-		move("/home/maria/bin/Frustratometer/frustratometer2/@sp[0]_@sp[1].pdb","@ARGV[0]/OutPut@ARGV[1]/PDB/@sp[0]_@sp[1].pdb");
-		copy("@ARGV[0]/OutPutFiles@ARGV[1]/Modeller/@sp[0]_@sp[1].pdb.done/FrustrationData/@sp[0]_@sp[1].pdb_singleresidue","@ARGV[0]/OutPutFiles@ARGV[1]/Modeller/@sp[0]_@sp[1].pdb.done/FrustrationData/@sp[0].pdb_singleresidue");
-		system("rm - r /home/maria/bin/Frustratometer/frustratometer2/@sp[0]_@sp[1].pdb.done");
+		system("cp @ARGV[0]/OutPutFiles@ARGV[1]/Modeller/@sp[0].pdb $pathFrustra/@sp[0]_@sp[1].pdb");
+		system ("cd $pathFrustra; perl RunFrustratometer.pl @sp[0]_@sp[1].pdb singleresidue");	
+		system("mv $pathFrustra/@sp[0]_@sp[1].pdb.done @ARGV[0]/OutPutFiles@ARGV[1]/Modeller/@sp[0]_@sp[1].pdb.done");
+		system("mv $pathFrustra/@sp[0]_@sp[1].pdb @ARGV[0]/OutPut@ARGV[1]/PDB/@sp[0]_@sp[1].pdb");
+		system("cp @ARGV[0]/OutPutFiles@ARGV[1]/Modeller/@sp[0]_@sp[1].pdb.done/FrustrationData/@sp[0]_@sp[1].pdb_singleresidue @ARGV[0]/OutPutFiles@ARGV[1]/Modeller/@sp[0]_@sp[1].pdb.done/FrustrationData/@sp[0].pdb_singleresidue");
+		system("rm -r /home/maria/frustratometer2/@sp[0]_@sp[1].pdb.done");
 		
 	}
 }	
@@ -45,6 +46,8 @@ system ("hmmbuild --amino @ARGV[0]/OutPutFiles@ARGV[1]/familias.hmm @ARGV[0]/Out
 system ("hmmsearch --domtblout @ARGV[0]/OutPutFiles@ARGV[1]/salida @ARGV[0]/OutPutFiles@ARGV[1]/familias.hmm @ARGV[0]/OutPutFiles@ARGV[1]/AlignClean.fasta");
 
 open(hmmsal,"@ARGV[0]/OutPutFiles@ARGV[1]/salida");
+
+#-- changes in alignment
 
 my $in=0;
 my $pdbidsal;
@@ -76,10 +79,11 @@ while (my $SEQ = $secuencia->next_seq()){
 
 close(alignA);
 
+
 open(align, "@ARGV[0]/OutPutFiles@ARGV[1]/SeqAlign.fasta");
 open(sal,">@ARGV[0]/OutPutFiles@ARGV[1]/SeqAlign2.fasta");
 
-print sal ">$pdbidsal\n$seq\n\n";
+print sal ">$pdbidsal\n$seq\n";
 
 while(my $line=<align>){
 	chomp $line;
@@ -104,12 +108,14 @@ while(my $line=<align>){
 close(align);
 close(sal);
 
+#--- Missing Residuos 
 
 if(@ARGV[2] eq "Y"){
 	system ("perl @ARGV[0]/Script/MissingComplete.pl @ARGV[0] @ARGV[1] @sp[2]");
 	system ("perl @ARGV[0]/Script/ChangeAlign.pl @ARGV[0] @ARGV[1]");
 }
 else{
-	copy("@ARGV[0]/OutPutFiles@ARGV[1]/SeqAlign2.fasta","@ARGV[0]/OutPutFiles@ARGV[1]/SeqAlign3.fasta");
-	copy("@ARGV[0]/OutPutFiles@ARGV[1]/SeqAlign2.fasta","@ARGV[0]/OutPutFiles@ARGV[1]/SeqAlign4.fasta");
+	system("cp @ARGV[0]/OutPutFiles@ARGV[1]/SeqAlign2.fasta @ARGV[0]/OutPutFiles@ARGV[1]/SeqAlign3.fasta");
+	system("cp @ARGV[0]/OutPutFiles@ARGV[1]/SeqAlign2.fasta @ARGV[0]/OutPutFiles@ARGV[1]/SeqAlign4.fasta");
 }
+
